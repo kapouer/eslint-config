9b80ca27662ae1f28c99bd93a8d397675e0a8733
Jérémy Lal <kapouer@melix.org>
2021-08-11 15:55:45 +0200

    No unsafe optional chaining

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ module.exports = {
 		"no-implicit-coercion": ["warn", { "disallowTemplateShorthand": true, "boolean": true, "number": true, "string": true }],
 		"no-multi-spaces": "error",
 		"no-implied-eval": "error",
+		"no-unsafe-optional-chaining": ["error", { "disallowArithmeticOperators": true }],
 		"yoda": "error",
 		"prefer-const": "error"
 	},
